7cac6845525e577e68616bd56511575aa5593d98
Wilfredo Mateo <wmateo86@gmail.com>
2024-11-12 10:53:50 -0600

    generate an image

--- example.mjs
+++ example.mjs
@@ -1,15 +1,6 @@
 import OpenAI from "openai";
 const openai = new OpenAI();
 
-const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        {
-            role: "user",
-            content: "Write a haiku about recursion in programming.",
-        },
-    ],
-});
+const image = await openai.images.generate({ prompt: "A cute baby sea otter" });
 
-console.log(completion.choices[0].message);
+console.log(image.data[0].url);
